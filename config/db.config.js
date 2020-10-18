0017a5e30c9ac35c9fd33779cf8e2d4ee25dc7bf
MikeLiam <miguelalcalaromero@gmail.com>
2020-10-18 14:01:42 +0200

    heroku

---
+++ config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // url: "mongodb://localhost:27017/bezkoder_db"
+    url: "mongodb+srv://MikeLiam:y1t2k2kITeaivBcj@cluster0.w3hyt.mongodb.net/nuxtfolio?retryWrites=true&w=majority"
+  };
